4cadaaf8b09ba05774d22a48a8f0e347099d5041
Angel Leu <angel840816@gmail.com>
2020-04-02 19:10:13 -0700

    update

--- routes/admin/updatePost.js
+++ routes/admin/updatePost.js
@@ -1,22 +1,31 @@
 const Product = require('../../model/product');
 
 const adminUpdate = async (req, res) => {
-
-    const idToUpdate = req.body.id;
-
+    try{
+    const idToUpdate = req.params.id;
+    
     const updateProduct = {
         name: req.body.name,
         description: req.body.description,
         price: req.body.price,
         image: req.file.filename
     }
-
+    if (req.file){
+        console.log("Updating Products");
+        updateProduct.name = req.body.name
+        updateProduct.description = req.body.description
+        updateProduct.price = req.body.price
+        updateProduct.filename = req.body.filename
+    }
     let filter = { _id: idToUpdate };
 
     let result = await Product.updateOne(filter, updateProduct).exec();
-    console.log("Result: ", result)
-
-    res.redirect('/admin');
+    //console.log("Result: ", result)
+    }catch(err){
+        console.log("ERR: ", err);
+    }finally {
+        res.redirect('/admin');
+    }
 }
 
 module.exports = adminUpdate; 
