37fa60d48a0b16a914a098395126c494b01b8ffc
Angel Leu <angel840816@gmail.com>
2020-04-05 13:43:24 -0700

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express =require('express')
 const app =express();
 const multer=require('multer');
 var bodyParser =require('body-parser')
-const port =process.env.port || 3000;
+const port =process.env.PORT || 3000;
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
